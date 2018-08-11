96f33afea0118bcf34b46d46e62c4037f2f76664
Garry Clerge <clergeg@gmail.com>
2018-08-11 15:47:40 -0400

    first commit

---
+++ tools/console.rb
@@ -0,0 +1,11 @@
+require_relative '../config/environment.rb'
+
+def reload
+  load 'config/environment.rb'
+end
+
+
+
+binding.pry
+
+"hello" #leave this here to ensure binding.pry isn't the last line
